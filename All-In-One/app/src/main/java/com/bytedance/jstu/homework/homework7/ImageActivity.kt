7eae24cd00720f7a9d97468c7356c9e6277ceaae
Abigail <81209800+Abigail1985@users.noreply.github.com>
2022-04-16 23:02:17 +0800

    add homework7-video basic

--- All-In-One/app/src/main/java/com/bytedance/jstu/homework/homework7/ImageActivity.kt
+++ All-In-One/app/src/main/java/com/bytedance/jstu/homework/homework7/ImageActivity.kt
@@ -21,27 +21,16 @@ class ImageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_image)
         viewPager = findViewById(R.id.media_view_pager)
 
-
-        addImage("https://www.2008php.com/2013_Website_appreciate/2013-12-15/20131215023521Of7AROf7AR.jpg")
+        addImage("https://p9.itc.cn/q_70/images03/20210317/947517063d494dafbaee811ad5c22d6e.gif")
+        addImage("https://p0.itc.cn/q_70/images03/20210317/78de632bd4c04b1b9ccc569e8a2b937d.gif")
         addImage("https://2-im.guokr.com/gkimage/kc/he/w5/kchew5.png")
         addImage("https://pics7.baidu.com/feed/0823dd54564e92586fa3c407c3bac15ecdbf4e06.jpeg?token=c2d11e393766ea2069e39a3e37863f98&s=2A82DC4D46896D57130DC92A03006053")
-        addImage("https://t7.baidu.com/it/u=4162611394,4275913936&fm=193&f=GIF")
+
 
         val adapter = ViewAdapter()
         adapter.setDatas(pages)
         viewPager.adapter = adapter
     }
-
-    private fun addImage(resId: Int) {
-        val imageView =
-            layoutInflater.inflate(R.layout.activity_base_multimedia_image_item, null) as ImageView
-        Glide.with(this)
-            .load(resId)
-            .error(R.drawable.error)
-            .into(imageView)
-        pages.add(imageView)
-    }
-
 
     private fun addImage(path: String) {
         val imageView =
--- All-In-One/app/src/main/java/com/bytedance/jstu/homework/homework7/VideoActivity.kt
+++ All-In-One/app/src/main/java/com/bytedance/jstu/homework/homework7/VideoActivity.kt
@@ -4,9 +4,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bytedance.jstu.homework.R
 
+import android.net.Uri
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_video.*
+
 class VideoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video)
+        val uri = Uri.parse("android.resource://$packageName/${R.raw.video}")
+        videoView.setVideoURI(uri)
+        play.setOnClickListener {
+            if (!videoView.isPlaying) {
+                videoView.start() // 开始播放
+            }
+            Log.d("MainActivity", "video is playing")
+        }
+        pause.setOnClickListener {
+            if (videoView.isPlaying) {
+                videoView.pause() // 暂停播放
+            }
+        }
+        replay.setOnClickListener {
+            if (videoView.isPlaying) {
+                videoView.resume() // 重新播放
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        videoView.suspend()
     }
 }
